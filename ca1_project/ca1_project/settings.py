c5fcde06a4d0e5c8124e1e0a5fd8ce9a6f307386
ImanRostam <c17367011@mydit.ie>
2020-11-10 01:53:35 +0000

    prepared for deploy

--- ca1_project/ca1_project/settings.py
+++ ca1_project/ca1_project/settings.py
@@ -21,10 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-env = environ.Env(
-    # set casting, default value
-    DEBUG=(bool, False)
-)
+env = environ.Env()
 
 # reading .env file
 environ.Env.read_env()
